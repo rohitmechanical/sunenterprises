4fccbb0c7f26357bea7da878b75f0861c4766104
sunrealfacility <neetu152@gmail.com>
2022-06-21 17:51:12 +0530

    deploy

--- website/settings.py
+++ website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-39k655=9p5%pz9-g7x^@wh)_x0gc6ikuhuetrqm!1smcn7vehb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
