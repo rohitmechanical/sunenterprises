ce6bdf5a4fadca6b46b5fa526b014fff3b53080b
rohitmechanical <rohit_mechanical@yahoo.com>
2022-07-11 03:15:27 +0530

    False

--- website/settings.py
+++ website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-39k655=9p5%pz9-g7x^@wh)_x0gc6ikuhuetrqm!1smcn7vehb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["sunfacilty.herokuapp.com/","127.0.0.1"]
 
